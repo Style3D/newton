e6f167712e7d96e40214e24de70752077b1a1ea7
Wenchao Huang <huangwenchao@linctex.com>
2025-07-03 15:39:57 +0800

    Wrapper classes for Warp's BVH.
    Signed-off-by: Wenchao Huang <huangwenchao@linctex.com>

---
+++ newton/solvers/style3d/plus/__init__.py
@@ -0,0 +1,9 @@
+########################################################################################################################
+#   Company:        Zhejiang Linctex Digital Technology Ltd.(Style3D)                                                  #
+#   Copyright:      All rights reserved by Linctex                                                                     #
+#   Description:    Style3D collision package                                                                          #
+#   Author:         Wenchao Huang (physhuangwenchao@gmail.com)                                                         #
+#   Date:           2025/07/03                                                                                         #
+########################################################################################################################
+
+__all__ = []
---
+++ newton/solvers/style3d/plus/bvh/__init__.py
@@ -0,0 +1,15 @@
+########################################################################################################################
+#   Company:        Zhejiang Linctex Digital Technology Ltd.(Style3D)                                                  #
+#   Copyright:      All rights reserved by Linctex                                                                     #
+#   Description:    Style3D BVH package                                                                                #
+#   Author:         Wenchao Huang (physhuangwenchao@gmail.com)                                                         #
+#   Date:           2025/07/03                                                                                         #
+########################################################################################################################
+
+from .bvh import Bvh, EdgeBvh, TriBvh
+
+__all__ = [
+    "Bvh",
+    "EdgeBvh",
+    "TriBvh",
+]
